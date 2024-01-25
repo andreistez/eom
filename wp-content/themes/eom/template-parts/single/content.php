794dc364bc094e9dc580c8c274d203cb763544a7
andrsweb <d3vv.proo@gmail.com>
2024-01-25 20:07:43 +0700

    Some changes in content.php and added styles

--- wp-content/themes/eom/template-parts/single/content.php
+++ wp-content/themes/eom/template-parts/single/content.php
@@ -19,13 +19,41 @@ $additional	= get_field( 'additional_info' );
 	<div class="container">
 		<div class="post-single-content">
 			<h1 class="h1"><?php the_title() ?></h1>
-
-			<?php
+            <div class="post-single-image">
+            <?php
 			if( has_post_thumbnail() )
 				echo get_the_post_thumbnail( $post_id, 'large' );
-
-			the_content();
 			?>
+            </div>
+            <div class="post-single-info-wrapper">
+                <div class="post-single-info">
+                <?php
+                if( $info ){
+                    ?>
+                    <div class="post-single-info-inner">
+                        <?php echo $info ?>
+                    </div>
+                    <?php
+                }              
+                ?>
+                </div>
+                <div class="post-single-additional-info">
+                    <?php
+                    if( $additional ){
+                        ?>
+                        <div class="post-single-info-inner">
+                            <?php echo $additional ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="post-single-content-inner">
+            <?php
+            the_content();
+            ?>
+            </div>
 		</div>
 	</div>
 </article><!-- .post-single -->
