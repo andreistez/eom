f0581feb1e5b6aa84507f55838cd764ec4977f3c
andrsweb <d3vv.proo@gmail.com>
2024-01-30 20:30:53 +0700

    Project deployment

--- wp-content/themes/eom/header.php
+++ wp-content/themes/eom/header.php
@@ -21,6 +21,14 @@ $site_desc = get_bloginfo( 'description', 'display' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
 	<!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="shortcut icon" href="favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 	<!-- /Favicon -->
 
 	<title>
@@ -42,14 +50,16 @@ $site_desc = get_bloginfo( 'description', 'display' );
 
 	<div class="wrapper">
 		<header class="header color">
-			<div class="header-inner" id="menu-lock">
-				<?php
-				wp_nav_menu( [
-					'theme_location'	=> 'header_menu',
-					'container'			=> 'nav',
-					'container_class'	=> 'header-nav'
-				] );
-				?>
-			</div>
+            <div class="container">
+                <div class="header-inner" id="menu-lock">
+                    <?php
+                    wp_nav_menu( [
+                        'theme_location'	=> 'header_menu',
+                        'container'			=> 'nav',
+                        'container_class'	=> 'header-nav'
+                    ] );
+                    ?>
+                </div>
+            </div>
 		</header>
 
