e5663f382c2b3ce712db608c63e8930c2fb42c0c
andrsweb <d3vv.proo@gmail.com>
2024-02-01 22:19:55 +0700

    Created global_text section

--- wp-content/themes/eom/page.php
+++ wp-content/themes/eom/page.php
@@ -11,6 +11,8 @@ get_header();
 
 // Hero styles and scripts.
 wp_enqueue_style( 'hero', THEME_URI . '/static/css/hero/hero.min.css', [], THEME_VERSION );
+// Global text styles and scripts.
+wp_enqueue_style( 'global_text', THEME_URI . '/static/css/global_text/global_text.min.css', [], THEME_VERSION );
 ?>
 
 <main class="main">
@@ -30,6 +32,16 @@ wp_enqueue_style( 'hero', THEME_URI . '/static/css/hero/hero.min.css', [], THEME
                 <video src="<?php echo THEME_URI ?>/src/video/bg.mp4" autoplay muted playsinline loop>
                         <source src="<?php echo THEME_URI ?>/src/video/bg.webm" type="video/webm">
                 </video>
+            </div>
+        </div>
+    </section>
+
+    <section class="global__text">
+        <div class="container">
+            <div class="global__text_wrapper">
+                <p>
+                    A partnership of two non-profits and two for-profits, the Economics of Mutuality Alliance works across the globe to advance stakeholder capitalism through the Economics of Mutuality operating model.
+                </p>
             </div>
         </div>
     </section>
