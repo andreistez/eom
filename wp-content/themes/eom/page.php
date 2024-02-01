4d2d21fa2d511cb0eb02b754afeabc3f01a40cbf
andrsweb <d3vv.proo@gmail.com>
2024-02-02 03:51:41 +0700

    created image_text section with mobile and reusable functionality

--- wp-content/themes/eom/page.php
+++ wp-content/themes/eom/page.php
@@ -13,6 +13,8 @@ get_header();
 wp_enqueue_style( 'hero', THEME_URI . '/static/css/hero/hero.min.css', [], THEME_VERSION );
 // Global text styles and scripts.
 wp_enqueue_style( 'global_text', THEME_URI . '/static/css/global_text/global_text.min.css', [], THEME_VERSION );
+// Image_text styles and scripts.
+wp_enqueue_style( 'image_text', THEME_URI . '/static/css/image_text/image_text.min.css', [], THEME_VERSION );
 ?>
 
 <main class="main">
@@ -42,6 +44,41 @@ wp_enqueue_style( 'global_text', THEME_URI . '/static/css/global_text/global_tex
                 <p>
                     A partnership of two non-profits and two for-profits, the Economics of Mutuality Alliance works across the globe to advance stakeholder capitalism through the Economics of Mutuality operating model.
                 </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="image__text terracota">
+        <div class="container">
+            <div class="image__text_wrapper">
+                <div class="image__text_left">
+                    <img src="<?php echo THEME_URI ?>/static/img/text-img.png" alt="">
+                </div>
+                <div class="image__text_right">
+                    <div class="image__text_inner">
+                        <div class="subtitle white">
+                            OUR APPROACH
+                        </div>
+                        <div class="image__text_info">
+                            <div class="image__text_heading">
+                                <h2 class="h2">
+                                    A better way of seeing and doing business
+                                </h2>
+                            </div>
+                            <div class="image__text_paragraphs">
+                                <p>
+                                    The Economics of Mutuality is a proven operating model that drives positive societal impact and superior business performance.
+                                </p>
+                                <p>
+                                    Powered by digital tools, it equips companies to create enduring mutual value for the benefit of multiple stakeholders by addressing complex challenges in society.
+                                </p>
+                                <p>
+                                    Practical and robust, it embeds purpose-driven strategies, metrics, and workflows into day-to-day business operations.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
