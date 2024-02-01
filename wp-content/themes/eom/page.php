3032862a50bca23cd1df015bba3991f95fbc56ff
andrsweb <d3vv.proo@gmail.com>
2024-02-01 21:36:49 +0700

    Created hero with mobile

--- wp-content/themes/eom/page.php
+++ wp-content/themes/eom/page.php
@@ -8,6 +8,9 @@
  */
 
 get_header();
+
+// Hero styles and scripts.
+wp_enqueue_style( 'hero', THEME_URI . '/static/css/hero/hero.min.css', [], THEME_VERSION );
 ?>
 
 <main class="main">
@@ -17,6 +20,19 @@ get_header();
 		the_content();
 	}
 	?>
+
+    <section class="hero">
+        <div class="container">
+            <div class="hero__wrapper">
+                <h1 class="h1">
+                    Working Together for the Common Good
+                </h1>
+                <video src="<?php echo THEME_URI ?>/src/video/bg.mp4" autoplay muted playsinline loop>
+                        <source src="<?php echo THEME_URI ?>/src/video/bg.webm" type="video/webm">
+                </video>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
