b927fcd72e7733e2552f750749aca15669fb90fd
andrsweb <d3vv.proo@gmail.com>
2024-02-06 13:56:37 +0700

    created other resources section

--- wp-content/themes/eom/page.php
+++ wp-content/themes/eom/page.php
@@ -40,6 +40,9 @@ wp_enqueue_style( 'members', THEME_URI . '/static/css/members/members.min.css', 
 
 // latest styles.
 wp_enqueue_style( 'latest', THEME_URI . '/static/css/latest/latest.min.css', [], THEME_VERSION );
+
+// resources styles.
+wp_enqueue_style( 'resources', THEME_URI . '/static/css/resources/resources.min.css', [], THEME_VERSION );
 ?>
 
 <main class="main">
@@ -920,6 +923,51 @@ wp_enqueue_style( 'latest', THEME_URI . '/static/css/latest/latest.min.css', [],
                         Visit our Latest page for more News and Content
                         <span>→</span>
                     </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="resources">
+        <div class="container">
+            <div class="resources__wrapper">
+                <h2 class="h2 violet">
+                    Other Resources
+                </h2>
+                <div class="resources__items">
+                    <div class="resources__item">
+                        <div class="resources__item_inner">
+                            <div class="resources__img">
+                                <img src="<?php echo THEME_URI ?>/static/img/res1.png" alt="">
+                            </div>
+                            <a href="#" class="resources__link dark flex-sb">
+                                Oxford Book
+                                <span>→</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="resources__item">
+                        <div class="resources__item_inner">
+                            <div class="resources__img">
+                                <img src="<?php echo THEME_URI ?>/static/img/res2.png" alt="">
+                            </div>
+                            <a href="#" class="resources__link dark flex-sb">
+                                Oxford Forum
+                                <span>→</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="resources__item">
+                        <div class="resources__item_inner">
+                            <div class="resources__img">
+                                <img src="<?php echo THEME_URI ?>/static/img/res3.png" alt="">
+                            </div>
+                            <a href="#" class="resources__link dark flex-sb">
+                                Knowledge Archive
+                                <span>→</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
