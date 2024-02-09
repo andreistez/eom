b05af5afbcd6b7bc04338e9d864a687c89ef9875
andrsweb <d3vv.proo@gmail.com>
2024-02-10 03:16:13 +0700

    More fixes and blog styles

--- wp-content/themes/eom/template-parts/single/content.php
+++ wp-content/themes/eom/template-parts/single/content.php
@@ -18,41 +18,164 @@ $additional	= get_field( 'additional_info' );
 <article class="post-single post-<?php echo esc_attr( $post_id ) ?>">
 	<div class="container">
 		<div class="post-single-content">
-			<h1 class="h1"><?php the_title() ?></h1>
             <div class="post-single-image">
-            <?php
-			if( has_post_thumbnail() )
-				echo get_the_post_thumbnail( $post_id, 'large' );
-			?>
-            </div>
-            <div class="post-single-info-wrapper">
-                <div class="post-single-info">
                 <?php
-                if( $info ){
-                    ?>
-                    <div class="post-single-info-inner">
-                        <?php echo $info ?>
-                    </div>
-                    <?php
-                }              
+                if( has_post_thumbnail() )
+                    echo get_the_post_thumbnail( $post_id, 'large' );
                 ?>
-                </div>
-                <div class="post-single-additional-info">
-                    <?php
-                    if( $additional ){
-                        ?>
-                        <div class="post-single-info-inner">
-                            <?php echo $additional ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                </div>
             </div>
             <div class="post-single-content-inner">
+                <div class="post-single-estimate">
+                    <div class="post-single-read">
+                        15 min read
+                    </div>
+                    <div class="post-single-date">
+                        Oxford, September 2023
+                    </div>
+                </div>
+                <h1 class="h1"><?php the_title() ?></h1>
+                <div class="post-single-info-wrapper">
+                    <div class="post-single-info">
+                    <?php
+                    if( $info ){
+                        ?>
+                        <div class="post-single-info-inner">
+                            <?php echo $info ?>
+                        </div>
+                        <?php
+                    }              
+                    ?>
+                    </div>
+                    <div class="post-single-info">
+                        <?php
+                        if( $additional ){
+                            ?>
+                            <div class="post-single-info-inner">
+                                <?php echo $additional ?>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                </div>
             <?php
             the_content();
             ?>
+            </div>
+            <div class="post-single-latest">
+                <h2>Other Articles</h2>
+                <div class="latest-items">
+                        <div class="latest-item">
+                            <div class="latest-inner">
+                                <div class="latest-case-wrapper">
+                                    <div class="latest-case">
+                                        5 min read
+                                    </div>
+                                    <div class="latest-case">
+                                        Feb 2024
+                                    </div>
+                                </div>
+                                <h5 class="h5">
+                                    Why I’m No Longer Calling Myself a Brand Purpose Consultant
+                                </h5>
+                                <p>
+                                    A Conversation With Francisca Larraín Marshall
+                                </p>
+                                <div class="latest-link-wrapper">
+                                    <a href="#">
+                                        Read on
+                                        <span>
+                                            →
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="latest-item">
+                            <div class="latest-inner">
+                                <div class="latest-case-wrapper">
+                                    <div class="latest-case">
+                                        5 min read
+                                    </div>
+                                    <div class="latest-case">
+                                        Feb 2024
+                                    </div>
+                                </div>
+                                <h5 class="h5">
+                                    Executive Education Program Americas and Europe
+                                </h5>
+                                <p>
+                                    Text here
+                                </p>
+                                <div class="latest-link-wrapper">
+                                    <a href="#">
+                                        Read on
+                                        <span>
+                                            →
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="latest-item">
+                            <div class="latest-inner">
+                                <div class="latest-case-wrapper">
+                                    <div class="latest-case">
+                                        5 min read
+                                    </div>
+                                    <div class="latest-case">
+                                        Feb 2024
+                                    </div>
+                                </div>
+                                <h5 class="h5">
+                                    Economics of Mutuality Foundation Norway Forum
+                                </h5>
+                                <p>
+                                    Text here
+                                </p>
+                                <div class="latest-link-wrapper">
+                                    <a class="latest-link" href="#">
+                                        Read on
+                                        <span>
+                                            →
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="latest-item">
+                            <div class="latest-inner">
+                                <div class="latest-case-wrapper">
+                                    <div class="latest-case">
+                                        5 min read
+                                    </div>
+                                    <div class="latest-case">
+                                        Feb 2024
+                                    </div>
+                                </div>
+                                <h5 class="h5">
+                                    Economics of Mutuality Foundation Korea Forum
+                                </h5>
+                                <p>
+                                    Text here
+                                </p>
+                                <div class="latest-link-wrapper">
+                                    <a href="#">
+                                        Read on
+                                        <span>
+                                            →
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="latest-bottom-link">
+                <a href="#">
+                    Visit our Latest page for more News and Content
+                    <span>→</span>
+                </a>
             </div>
 		</div>
 	</div>
