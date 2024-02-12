5a4c0c3ddaaecbd822359a7894b10dcf94e7fc62
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-02-13 01:37:00 +0300

    upd: ImageText section.

--- wp-content/themes/eom/acf-flexible-content/image_text/text-block.php
+++ wp-content/themes/eom/acf-flexible-content/image_text/text-block.php
@@ -20,6 +20,7 @@ $title_size		= ( isset( $block['title_size'] ) && $block['title_size'] ) ? $bloc
 $text			= ( isset( $block['text'] ) && $block['text'] ) ? $block['text'] : '';
 $text_color		= ( isset( $block['text_color'] ) && $block['text_color'] ) ? $block['text_color'] : '#fff';
 $text_type		= ( isset( $block['text_type'] ) && $block['text_type'] ) ? ' ' . esc_attr( $block['text_type'] ) : '';
+$link			= ( isset( $block['link'] ) && $block['link'] ) ? $block['link'] : '';
 ?>
 
 <div class="image__text_info<?php echo $text_type ?>">
@@ -40,10 +41,22 @@ $text_type		= ( isset( $block['text_type'] ) && $block['text_type'] ) ? ' ' . es
 		echo '</div>';
 	}
 
-	if( $text )
-		echo '<div class="image__text_paragraphs" style="color: ', esc_attr( $text_color ), '">',
-			$text,
-		'</div>';
+	if( $text ){
+		echo '<div class="image__text_paragraphs" style="color: ', esc_attr( $text_color ), '">', $text;
+
+		if( $link ){
+			$link_url	= $link['url'];
+			$link_title	= $link['title'];
+			$target		= $link['target'] ? ' target="_blank"' : '';
+			?>
+			<a href="<?php echo esc_url( $link_url ) ?>"<?php echo $target ?>>
+				<?php echo esc_html( $link_title ) ?> <span>→</span>
+			</a>
+			<?php
+		}
+
+		echo '</div>';
+	}
 	?>
 </div>
 
