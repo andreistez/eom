a4c7119654e10054bfd61962198dfae1491e80d7
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-02-24 00:39:21 +0300

    feat: add Main Video Vimeo player.

--- wp-content/themes/eom/acf-flexible-content/main_video/main_video.php
+++ wp-content/themes/eom/acf-flexible-content/main_video/main_video.php
@@ -14,17 +14,29 @@ if( ! $video = get_sub_field( 'video' ) ) return;
 wp_enqueue_style( 'main-video', THEME_URI . '/static/css/main_video/main_video.min.css', [], THEME_VERSION );
 wp_enqueue_script( 'main-video', THEME_URI . '/static/js/main_video/main_video.min.js', ['jquery'], THEME_VERSION, true );
 
-$poster	= get_sub_field( 'poster' );
-$poster	= $poster ? ' poster="' . wp_get_attachment_image_url( $poster, 'video-poster@2x' ) . '"' : '';
+$video_type	= get_sub_field( 'video_type' );	// 'local' | 'vimeo'
+$vimeo_url	= get_sub_field( 'vimeo_url' );
+$poster		= get_sub_field( 'poster' );
+$poster		= $poster ? ' poster="' . wp_get_attachment_image_url( $poster, 'video-poster@2x' ) . '"' : '';
 ?>
 
 <section class="main__video">
 	<div class="container">
 		<div class="main__video_wrapper">
-			<video playsinline<?php echo $poster ?>>
-				<source src="<?php echo esc_url( $video['url'] ) ?>" type="<?php echo esc_attr( $video['mime_type'] ) ?>">
-			</video>
-			<div class="play"></div>
+			<?php
+			if( $video_type === 'local' ){
+				?>
+				<video playsinline<?php echo $poster ?>>
+					<source src="<?php echo esc_url( $video['url'] ) ?>" type="<?php echo esc_attr( $video['mime_type'] ) ?>">
+				</video>
+				<div class="play"></div>
+				<?php
+			}else{
+				?>
+				<div data-plyr-provider="vimeo" data-plyr-embed-id="<?php echo esc_url( $vimeo_url ) ?>"></div>
+				<?php
+			}
+			?>
 		</div>
 	</div>
 </section>
