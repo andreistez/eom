54fbccac34f1b4c8858d9e0bcde9e7c8e5775ccd
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-02-29 00:33:06 +0300

    feat: add Archive template with pagination.

---
+++ wp-content/themes/eom/archive.php
@@ -0,0 +1,39 @@
+<?php
+
+/** Category archive default template.
+ *
+ * @package WordPress
+ * @subpackage eom
+ */
+
+get_header();
+?>
+
+<main class="main">
+	<section class="archive-wrap">
+		<div class="container">
+			<h1 class="archive-title"><?php single_cat_title() ?></h1>
+
+			<?php
+			if( have_posts() ){
+				echo '<div class="archive-posts">';
+
+				while( have_posts() ){
+					the_post();
+					get_template_part( 'components/cards/post', null, ['id' => get_the_ID(), 'type' => 'thumb'] );
+				}
+
+				echo '</div>';
+
+				the_posts_pagination( [
+					'screen_reader_text' => ' '
+				] );
+			}
+			?>
+		</div>
+	</section>
+</main>
+
+<?php
+get_footer();
+
