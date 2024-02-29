19ea3615ff77308b9600b323dbccc07e1b1b39ab
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-02-29 23:44:23 +0300

    fix: person links.

--- wp-content/themes/eom/components/person-socials.php
+++ wp-content/themes/eom/components/person-socials.php
@@ -13,15 +13,15 @@ if( ! $social_links = $args['links'] ?? null ) return;
 <div class="person-links">
 	<?php
 	foreach( $social_links as $link ){
-	$url = $link['link']['url'];
-	$title = $link['link']['title'];
-	$target = $link['link']['target'] ? ' target="_blank"' : '';
+		$url	= $link['link']['url'] ?? '#';
+		$title	= $link['link']['title'] ?? '';
+		$target	= ( isset( $link['link']['target'] ) && $link['link']['target'] ) ? ' target="_blank"' : '';
+		?>
+		<a href="<?php echo esc_url( $url ) ?>"<?php echo $target ?>>
+			<?php echo esc_html( $title ) ?>
+		</a>
+		<?php
+	}
 	?>
-	<a href="<?php echo esc_url( $url ) ?>"<?php echo $target ?>>
-		<?php echo esc_html( $title ) ?>
-	</a>
-	<?php
-}
-?>
 </div>
 
