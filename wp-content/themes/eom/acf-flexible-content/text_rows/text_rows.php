d99358d526ea8c4e691462548a2badd1dac3cab4
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-03-04 00:57:32 +0300

    feat: add backend for TextRows and TextImage sections.

--- wp-content/themes/eom/acf-flexible-content/text_rows/text_rows.php
+++ wp-content/themes/eom/acf-flexible-content/text_rows/text_rows.php
@@ -9,31 +9,48 @@
  * @subpackage eom
  */
 
-wp_enqueue_style( 'text-rows', THEME_URI . '/static/css/text_rows/text_rows.min.css', [], THEME_VERSION );
+wp_enqueue_style( 'text_rows', THEME_URI . '/static/css/text_rows/text_rows.min.css', [], THEME_VERSION );
+wp_enqueue_script( 'text_rows', THEME_URI . '/static/js/text_rows/text_rows.min.js', ['jquery'], THEME_VERSION, true );
 
-$type			= get_sub_field( 'type' ) ?: '';
-$type			= $type ? ' ' . esc_attr( $type ) : '';
-$bg_color		= get_sub_field( 'bg_color' );
-$bg				= $bg_color ? ' style="background-color:' . esc_attr( $bg_color ) . '"' : '';
-$text_blocks	= get_sub_field( 'text_blocks' );
+$bg_color			= get_sub_field( 'bg_color' );
+$bg					= $bg_color ? ' style="background-color:' . esc_attr( $bg_color ) . '"' : '';
+$subtitle			= get_sub_field( 'subtitle' );
+$subtitle_color		= get_sub_field( 'subtitle_color' ) ?: '#000';
+$title				= get_sub_field( 'title' );
+$title_color		= get_sub_field( 'title_color' ) ?: '#000';
+$text_left			= get_sub_field( 'text_left' );
+$text_left_color	= get_sub_field( 'text_left_color' ) ?: '#000';
+$text_right			= get_sub_field( 'text_right' );
+$text_right_color	= get_sub_field( 'text_right_color' ) ?: '#000';
 ?>
 
-<section class="text__rows<?php echo $type ?>">
+<section class="text__rows">
 	<div class="container">
-        <div class="text__rows_wrapper">
-            <div class="text__rows_inner"<?php echo $bg ?>>
-                <?php
-                if( ! empty( $text_blocks ) ){
-                    ?>
-                    <?php
-                    foreach( $text_blocks as $block )
-                        get_template_part( 'acf-flexible-content/image_text/text-block', null, ['block' => $block] );
-                    ?>
-                    <?php
-                }
-                ?>
-            </div>
-        </div>
+		<div class="text__rows_wrapper"<?php echo $bg ?>>
+			<?php
+			if( $subtitle || $title ){
+				echo '<div class="image__text_heading">';
+
+				if( $subtitle ) echo '<p style="color:', esc_attr( $subtitle_color ), '">', esc_html( $subtitle ), '</p>';
+
+				if( $title ) echo '<h2 class="h2" style="color:', esc_attr( $title_color ), '">', esc_html( $title ), '</h2>';
+
+				echo '</div>';
+			}
+
+			if( $text_left || $text_right ){
+				echo '<div class="text__rows_blocks">';
+
+				if( $text_left )
+					echo '<div class="text__rows_block" style="color:', esc_attr( $text_left_color ), '">', $text_left, '</div>';
+
+				if( $text_right )
+					echo '<div class="text__rows_block" style="color:', esc_attr( $text_right_color ), '">', $text_right, '</div>';
+
+				echo '</div>';
+			}
+			?>
+		</div>
 	</div>
 </section>
 
--- wp-content/themes/eom/page-templates/acf-flexible-content.php
+++ wp-content/themes/eom/page-templates/acf-flexible-content.php
@@ -7,71 +7,10 @@
  * @subpackage eom
  */
 
-wp_enqueue_style( 'text_image', THEME_URI . '/static/css/text_image/text_image.min.css', [], THEME_VERSION );
-wp_enqueue_script( 'text_image', THEME_URI . '/static/js/text_image/text_image.min.js', ['jquery'], THEME_VERSION, true );
-
-wp_enqueue_style( 'text_rows', THEME_URI . '/static/css/text_rows/text_rows.min.css', [], THEME_VERSION );
-wp_enqueue_script( 'text_rows', THEME_URI . '/static/js/text_rows/text_rows.min.js', ['jquery'], THEME_VERSION, true );
-
 get_header();
 ?>
 
 <main class="main">
-<section class="text__image">
-        <div class="container">
-            <div class="text__image_wrapper">
-                <div class="text__image_info">
-                    <div class="image__text_heading">
-                        <p>
-                            xx
-                        </p>
-                        <h2 class="h2">
-                            Addressing Wicked Problems
-                        </h2>
-                    </div>
-                    <p>
-                        Many of the issues we’re facing in society today are so-called ‘wicked’ problems. These complex, systemic issues can’t be solved by one sector alone. They need stakeholders to come together from the public, private, and non-profit spheres. <br>
-                        The Economics of Mutuality Alliance is uniquely positioned to orchestrate cross-sector actors to find good solutions to seemingly intractable problems... Profitable solutions that are good for business and good for society.
-                    </p>
-                </div>
-                <div class="text__image_item">
-                    <img src="<?php echo THEME_URI ?>/static/img/wicked.svg" alt="">
-                </div>
-            </div>
-        </div>
-</section>
-<section class="text__rows">
-    <div class="container">
-        <div class="text__rows_wrapper">
-            <div class="image__text_heading">
-                <p>
-                    OUR APPROACH
-                </p>
-                <h2 class="h2">
-                    Our role and value
-                </h2>
-            </div>
-            <div class="text__rows_blocks">
-                <div class="text__rows_block">
-                    <p>
-                        Investment houses and family offices are charged with a duty to preserve and build wealth. That’s why they value general partners who can deploy and amplify their funds with wisdom and acuity. But that’s only half the story for forward-looking funds managers.
-                    </p>
-                    <p>
-                        In addition to receiving superior returns, they want to put their money to work to achieve long-term positive outcomes for people and for the planet. Because if they don’t solve problems profitably and focus on the future, they - and we - won’t have one.
-                    </p>
-                </div>
-                <div class="text__rows_block">
-                    <p>
-                        And that’s where we come in. Mutual Value Investments identifies and realizes opportunities for sustainable growth, based on the principle of mutual value creation. We help turn societal problems into profitable opportunities.
-                    </p>
-                    <p>
-                        We create enduring mutual value by identifying opportunities; raising dedicated equity from values-aligned investors; and operationalizing the Economics of Mutuality model to transform the business and improve performance across multiple capitals.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 	<?php
 	/**
 	 * 1. Please create ACF Flexible Content setting
