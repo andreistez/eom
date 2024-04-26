5f56f0546c510c165b1292cbeaefe233c580ae10
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-04-27 01:08:39 +0300

    upd: add Hero subtitle color.

--- wp-content/themes/eom/acf-flexible-content/hero/hero.php
+++ wp-content/themes/eom/acf-flexible-content/hero/hero.php
@@ -3,38 +3,42 @@
 /**
  * Hero - ACF Flexible Content section layout.
  *
- * @see Page with ACF Flexible Content template -> Flexible Content -> Hero
+ * @see        Page with ACF Flexible Content template -> Flexible Content -> Hero
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage eom
  */
 
 wp_enqueue_style( 'hero', THEME_URI . '/static/css/hero/hero.min.css', [], THEME_VERSION );
 
-$bg_type		= get_sub_field( 'background_type' ) ?: 'video';
-$video_bg		= get_sub_field( 'video_bg' );
-$image			= get_sub_field( 'image' );
-$subtitle		= get_sub_field( 'subtitle' );
-$title			= get_sub_field( 'title' );
-$title_color	= get_sub_field( 'title_color' ) ?: '#fff';
+$bg_type        = get_sub_field( 'background_type' ) ?: 'video';
+$video_bg       = get_sub_field( 'video_bg' );
+$image          = get_sub_field( 'image' );
+$subtitle       = get_sub_field( 'subtitle' );
+$title          = get_sub_field( 'title' );
+$title_color    = get_sub_field( 'title_color' ) ?: '#fff';
+$subtitle_color = get_sub_field( 'subtitle_color' ) ?: '#fff';
 ?>
 
 <section class="hero">
 	<div class="container">
 		<div class="hero__wrapper">
 			<?php
-			if( $bg_type === 'image' && $image )
-				echo '<div class="hero-bg">', wp_get_attachment_image( $image, 'video-poster' ), '</div>';
+			if( $bg_type === 'image' &&
+			    $image ) echo '<div class="hero-bg">', wp_get_attachment_image( $image, 'video-poster' ), '</div>';
 
-			if( $subtitle )
-				echo '<div class="hero-subtitle" style="color: ', esc_attr( $title_color ), '">', $subtitle, '</div>';
+			if( $subtitle ) echo '<div class="hero-subtitle" style="color: ', esc_attr( $subtitle_color ), '">', $subtitle, '</div>';
 
-			if( $title )
-				echo '<h1 class="h1" style="color: ', esc_attr( $title_color ), '">', $title, '</h1>';
+			if( $title ) echo '<h1 class="h1" style="color: ', esc_attr( $title_color ), '">', $title, '</h1>';
 
 			if( $bg_type === 'video' && $video_bg ){
 				?>
-				<video autoplay muted playsinline loop>
+				<video
+					autoplay
+					muted
+					playsinline
+					loop
+				>
 					<source
 						src="<?php echo esc_url( $video_bg['url'] ) ?>"
 						type="<?php echo esc_attr( $video_bg['mime_type'] ) ?>"
