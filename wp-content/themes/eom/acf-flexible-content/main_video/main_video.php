e1c4252ba2144675f2044fd682ba60779ced50a8
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-04-27 00:15:46 +0300

    upd: MainVideo left Vimeo only.

--- wp-content/themes/eom/acf-flexible-content/main_video/main_video.php
+++ wp-content/themes/eom/acf-flexible-content/main_video/main_video.php
@@ -13,51 +13,25 @@ wp_enqueue_style( 'main-video', THEME_URI . '/static/css/main_video/main_video.m
 wp_enqueue_script( 'main-video', THEME_URI .
                                  '/static/js/main_video/main_video.min.js', [ 'jquery' ], THEME_VERSION, true );
 
-$video     = get_sub_field( 'video' );
-$vimeo_url = get_sub_field( 'vimeo_url' );
+if( ! $vimeo_url = get_sub_field( 'vimeo_url' ) ) return;
 
-if( ! $video && ! $vimeo_url ) return;
-
-$video_type = get_sub_field( 'video_type' );    // 'local' | 'vimeo'
 $poster     = get_sub_field( 'poster' );
-$poster     = $poster ? ' poster="' . wp_get_attachment_image_url( $poster, 'video-poster@2x' ) . '"' : '';
 $video_desc = get_sub_field( 'video_desc' );
 ?>
 
 <section class="main__video">
 	<div class="container">
 		<div class="main__video_wrapper">
-			<?php
-			if( $video_type === 'local' ){
-				?>
-				<div class="main__video_wrap">
-					<video playsinline<?php echo $poster ?>>
-						<source
-							src="<?php echo esc_url( $video['url'] ) ?>"
-							type="<?php echo esc_attr( $video['mime_type'] ) ?>"
-						>
-					</video>
-
-					<?php if( $video_desc ) echo '<div class="video__desc">', $video_desc, '</div>' ?>
-				</div>
+			<div
+				class="embededVideo"
+				data-id-vimeo="<?php echo esc_url( $vimeo_url ) ?>"
+			>
 				<?php
-			}
-			else{
-				?>
-				<div
-					class="embededVideo"
-					data-id-vimeo="<?php echo esc_url( $vimeo_url ) ?>"
-				>
-					<img
-						src="<?php echo THEME_URI ?>/static/img/bg.jpg"
-						alt=""
-					>
+				if( $poster ) echo wp_get_attachment_image( $poster, 'video-poster@2x', false, [ 'loading' => 'lazy' ] );
 
-					<?php if( $video_desc ) echo '<div class="video__desc">', $video_desc, '</div>' ?>
-				</div>
-				<?php
-			}
-			?>
+				if( $video_desc ) echo '<div class="video__desc">', $video_desc, '</div>';
+				?>
+			</div>
 			<div class="play"></div>
 		</div>
 	</div>
