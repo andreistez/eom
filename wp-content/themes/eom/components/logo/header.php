a45ddd890bafa9c50bcfb515c9cb89b9bab38979
andrsweb <d3vv.proo@gmail.com>
2024-06-06 00:24:35 +0700

    Added layout of new "Exec" template

--- wp-content/themes/eom/components/logo/header.php
+++ wp-content/themes/eom/components/logo/header.php
@@ -11,7 +11,10 @@
 <div class="header__logo_wrapper">
 	<a href="/" class="logo">
 		<div class="logo__inner">
-			<?php get_template_part( 'components/logo/gears-and-text', null, ['text' => 'Economics<br/>of mutuality<br/>alliance'] ) ?>
+			<?php get_template_part( 'components/logo/gears-and-text', null, ['text' => 'Mutual<br/>Value<br/>Labs'] ) ?>
+			<div class="logo__inner_img">
+				<img src="<?php echo THEME_URI ?>/static/img/logos.png" alt="">
+			</div>
 		</div>
 	</a>
 </div>
