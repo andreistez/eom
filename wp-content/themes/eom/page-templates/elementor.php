0ed3fdb7f6f121da89780b5d611d161cc4575bc2
Andrei Stezenko <astezenko@markupus.tech>
2024-07-17 12:10:51 +0300

    fix: delete thumb

--- wp-content/themes/eom/page-templates/elementor.php
+++ wp-content/themes/eom/page-templates/elementor.php
@@ -13,15 +13,7 @@ get_header();
 <main class="main">
 	<section class="elementor-wrap">
 		<div class="elementor-content">
-			<?php
-			if( has_post_thumbnail() ){
-				echo '<div class="elementor-content-thumb">';
-				the_post_thumbnail();
-				echo '</div>';
-			}
-
-			the_content();
-			?>
+			<?php the_content() ?>
 		</div>
 	</section>
 </main>
